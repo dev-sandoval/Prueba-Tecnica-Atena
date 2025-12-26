c4841bca50798fb51ff2a02f4f8b89e13b0fd0fc
dev-sandoval <davidsandoval.dev@gmail.com>
2025-12-26 10:54:32 -0500

    ✨ agregar clase ApiResponse para manejar respuestas de API

---
+++ UserManagement.API/Models/ApiResponse.cs
@@ -0,0 +1,30 @@
+﻿namespace UserManagement.API.Models;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T? Data { get; set; }
+
+    public ApiResponse() { }
+
+    public static ApiResponse<T> Ok(T data, string message = "Operación exitosa")
+    {
+        return new ApiResponse<T>
+        {
+            Success = true,
+            Data = data,
+            Message = message
+        };
+    }
+
+    public static ApiResponse<T> Fail(string errorMessage)
+    {
+        return new ApiResponse<T>
+        {
+            Success = false,
+            Data = default,
+            Message = errorMessage
+        };
+    }
+}
