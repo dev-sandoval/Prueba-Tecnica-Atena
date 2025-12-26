25eb6ac62055cedd23eee335673a0be7ea821b1b
David Sandoval <davidsandoval.dev@gmail.com>
2025-12-26 17:09:21 -0500

    ♻️ reorganizar configuración de Swagger y controladores en Program.cs

--- UserManagement.API/Program.cs
+++ UserManagement.API/Program.cs
@@ -5,6 +5,11 @@ using UserManagement.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(CreateUserCommand).Assembly));
 
@@ -20,9 +25,13 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
 });
 
-builder.Services.AddControllers();
-
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseCors("AllowAngular");
 app.UseHttpsRedirection();
