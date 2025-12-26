750643da6bea241be973b21f7661cc4ddd95f089
dev-sandoval <davidsandoval.dev@gmail.com>
2025-12-26 10:43:07 -0500

    ✨ agregar comando CreateUser para manejo de creación de usuarios

---
+++ UserManagement.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using UserManagement.Application.Common;
+
+namespace UserManagement.Application.Users.Commands.CreateUser
+{
+    public record CreateUserCommand(string FirstName, string LastName, string Email, string Phone)
+        : IRequest<Result<Guid>>;
+}
