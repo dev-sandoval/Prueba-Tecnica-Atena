f5722c65b9459c2155ea638a8db750c8f4ffc606
dev-sandoval <davidsandoval.dev@gmail.com>
2025-12-26 10:55:38 -0500

    ✨ agregar controlador UsersController para gestionar usuarios

---
+++ UserManagement.API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.API.Models;
+using UserManagement.Application.Users.Commands.CreateUser;
+using UserManagement.Application.Users.Queries.GetAll;
+
+namespace UserManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse<List<UserDto>>>> GetAll()
+        {
+            var result = await mediator.Send(new GetAllUsersQuery());
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(ApiResponse<List<UserDto>>.Fail(result.Error));
+            }
+
+            return Ok(ApiResponse<List<UserDto>>.Ok(result.Value!, "Usuarios cargados correctamente"));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse<Guid>>> Create([FromBody] CreateUserCommand command)
+        {
+            var result = await mediator.Send(command);
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(ApiResponse<Guid>.Fail(result.Error));
+            }
+
+            return Ok(ApiResponse<Guid>.Ok(result.Value, "Usuario creado exitosamente"));
+        }
+    }
+}
