7ec89dc8542c15458e79efef5f77ded2fa71da96
dev-sandoval <davidsandoval.dev@gmail.com>
2025-12-26 10:47:37 -0500

    ✨ agregar clase ApplicationDbContext para la gestión de usuarios

---
+++ UserManagement.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace UserManagement.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(builder);
+        }
+    }
+}
