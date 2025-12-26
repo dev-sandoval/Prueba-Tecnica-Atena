a1d770ac8b303fe817e610d191fd7c1fbc31c853
dev-sandoval <davidsandoval.dev@gmail.com>
2025-12-26 10:47:45 -0500

    ✨ agregar configuración de entidad User para la gestión de usuarios

---
+++ UserManagement.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace UserManagement.Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+            builder.Property(u => u.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.LastName).IsRequired().HasMaxLength(50);
+            builder.Property(u => u.Email).IsRequired().HasMaxLength(100);
+            builder.Property(u => u.Phone).IsRequired().HasMaxLength(20);
+            builder.Property(u => u.CreatedAt).IsRequired();
+            builder.HasIndex(u => u.Email).IsUnique();
+        }
+    }
+}
