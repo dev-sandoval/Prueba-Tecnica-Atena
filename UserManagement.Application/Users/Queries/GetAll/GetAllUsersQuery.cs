c82c2838d07f5e917c9647886ac11a794e8f4976
dev-sandoval <davidsandoval.dev@gmail.com>
2025-12-26 10:44:28 -0500

    ✨ agregar consulta GetAllUsers para obtener todos los usuarios

---
+++ UserManagement.Application/Users/Queries/GetAll/GetAllUsersQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using UserManagement.Application.Common;
+
+namespace UserManagement.Application.Users.Queries.GetAll
+{
+    public record UserDto(Guid Id, string FirstName, string LastName, string Email, string Phone, DateTime CreatedAt);
+
+    public record GetAllUsersQuery : IRequest<Result<List<UserDto>>>;
+}
