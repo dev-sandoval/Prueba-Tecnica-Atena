3968fd4b2910199634fde061c8242cac923bfc07
dev-sandoval <davidsandoval.dev@gmail.com>
2025-12-26 10:44:41 -0500

    ✨ agregar manejador de consulta GetAllUsers para obtener todos los usuarios

---
+++ UserManagement.Application/Users/Queries/GetAll/GetAllUsersQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Application.Common;
+using UserManagement.Application.Interfaces;
+
+namespace UserManagement.Application.Users.Queries.GetAll;
+
+public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, Result<List<UserDto>>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetAllUsersQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Result<List<UserDto>>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+    {
+        var users = await _context.Users
+            .AsNoTracking()
+            .Select(u => new UserDto(
+                u.Id,
+                u.FirstName,
+                u.LastName,
+                u.Email,
+                u.Phone,
+                u.CreatedAt))
+            .ToListAsync(cancellationToken);
+
+        return Result<List<UserDto>>.Success(users);
+    }
+}
