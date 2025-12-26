51aabd21dd3fdc9ace24c65f4faf1fec18b626f9
dev-sandoval <davidsandoval.dev@gmail.com>
2025-12-26 10:41:09 -0500

    ✨ agregar interfaz IApplicationDbContext para manejo de contexto de base de datos

---
+++ UserManagement.Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
